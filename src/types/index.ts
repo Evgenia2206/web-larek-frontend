667e93e7a3392f1d4a993178510f50db8df110f8
Evgenia Ivleva <valkiriya-one@mail.ru>
2024-11-01 16:57:42 +0300

    intermediate_4

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,14 @@
 import { ApiPostMethods } from '../components/base/api'
 
+// Тип способа оплаты
+type TPayment = 'online' | 'receipt';
+
+// Тип категории товара
+type TCategory = 'другое' | 'софт-скил'	| 'дополнительное'	| 'кнопка'	| 'хард-скил';
+
+// Тип ошибки в форме
+type TFormErrors = Partial<Record<keyof IOrder, string>>;
+
 // Интерфейс API-клиента
 interface IApi {
 	baseUrl: string;
@@ -15,6 +24,7 @@ interface IProduct {
     description: string;
     image: string;
     price: number | null;
+    selected: boolean;
 }
 
 // Интерфейс для работы с корзиной
@@ -32,5 +42,30 @@ interface IOrder {
 	phone: string; 
 }
 
+// Интерфейс главной страницы товаров
+interface IMainPage {
+    cartCounter: number;
+    products: HTMLElement[];
+    locked: boolean;
+}
 
+// Интерфейс модального окна
+interface IModal {
+    content: HTMLElement;
+}
 
+// Интерфейс проверки заполнения формы
+interface IFormState {
+    valid: boolean;
+    errors: string[];
+}
+
+// Интерфейс подтверждения заказа
+interface IOrderProcessed {
+	orderCost: number;
+}
+
+// Интерфейс продолжения работы с приложением
+interface IOrderProcessedActions {
+    onClick: () => void;
+}
